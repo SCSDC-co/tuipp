ed2cf999499d15ee12ea0de0b5969d46743d6ff5
Sckab <giulianodeamicis2009@gmail.com>
2026-04-29 20:51:49 +0200

    docs: add other options in the library docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,16 +3,26 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "TUI++",
+
   description:
     "A simple TUI library heavily inspired by rich and Spectre.Console",
+
   cleanUrls: true,
+
   appearance: "force-dark",
+
+  lastUpdated: true,
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "Home", link: "/" },
       { text: "Examples", link: "/markdown-examples" },
     ],
+
+    editLink: {
+      pattern: "https://github.com/SCSDC-co/tuipp/edit/main/docs/:path",
+    },
 
     sidebar: [
       {
